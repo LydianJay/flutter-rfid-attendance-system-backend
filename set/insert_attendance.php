7ce0fc4aab80206875694f01450f1c3ae03f7ef8
LydianJay <lloydjayedradan@gmail.com>
2024-09-13 23:23:31 +0800

    stuff

--- set/insert_attendance.php
+++ set/insert_attendance.php
@@ -26,38 +26,55 @@ if ($data === null) {
     echo json_last_error_msg();
 } else {
 
-    $date   = date('m-d-Y');
-    $splite = explode('-', $date);
-    $time   = date('H:i');
+    // valid rfid's are ids that are associated with an account, need to check on that
 
-    $month  = $splite[0];
-    $day    = $splite[1];
-    $year   = $splite[2];
+    $q3     = "SELECT COUNT(rfid) as n FROM students WHERE rfid = '$data->rfid' ";
+    $q3res  = $conn->query($q3);
+    $q3as   = $q3res->fetch_assoc();
+
+    if($q3as['n'] >= 1) { // account with rfid is associated
 
 
-    // count how if already has sign in and out for the day
-
-    $q2     = "SELECT COUNT(rfid) as n FROM attendance WHERE rfid = '$data->rfid' AND day = '$day' AND month = '$month' AND year = '$year' ";
-    $q2res  = $conn->query($q2);
-    $q2as   = $q2res->fetch_assoc();
-    // ================================================
+        $date   = date('m-d-Y');
+        $splite = explode('-', $date);
+        $time   = date('H:i');
     
-    if($q2as['n'] < 2){
-        $find = "SELECT COUNT(rfid) AS n FROM attendance WHERE rfid = '$data->rfid' AND day = '$day' AND month = '$month' AND year = '$year' AND type = 1 ";
-        $fRes = $conn->query($find);
-        $fjson = $fRes->fetch_assoc();
-
-
-        $type = $fjson['n'] >= 1 ? 0 : 1;
+        $month  = $splite[0];
+        $day    = $splite[1];
+        $year   = $splite[2];
     
+    
+        // count how if already has sign in and out for the day
+    
+        $q2     = "SELECT COUNT(rfid) as n FROM attendance WHERE rfid = '$data->rfid' AND day = '$day' AND month = '$month' AND year = '$year' ";
+        $q2res  = $conn->query($q2);
+        $q2as   = $q2res->fetch_assoc();
+        // ================================================
         
-        $sql = "
-        INSERT INTO attendance (rfid, day, month, year, time, type) 
-        VALUES ('$data->rfid', '$day', '$month', '$year', '$time', '$type')
-        ";
-        $conn->query($sql);
-    }
+        if($q2as['n'] < 2){
+            $find = "SELECT COUNT(rfid) AS n FROM attendance WHERE rfid = '$data->rfid' AND day = '$day' AND month = '$month' AND year = '$year' AND type = 1 ";
+            $fRes = $conn->query($find);
+            $fjson = $fRes->fetch_assoc();
     
+    
+            $type = $fjson['n'] >= 1 ? 0 : 1;
+        
+            
+            $sql = "
+            INSERT INTO attendance (rfid, day, month, year, time, type) 
+            VALUES ('$data->rfid', '$day', '$month', '$year', '$time', '$type')
+            ";
+            $conn->query($sql);
+        }
+        
+
+
+
+    }
+
+    // ==============================================================================================
+
+   
     
    
 }
