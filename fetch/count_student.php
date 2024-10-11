6997143acdb461014697c49c98833630df1bffb0
LydianJay <lloydjayedradan@gmail.com>
2024-10-12 00:22:41 +0800

    some fixes

--- fetch/count_student.php
+++ fetch/count_student.php
@@ -13,7 +13,7 @@ if (mysqli_connect_error()) {
     echo "Error";
     die("Connection failed: " . $conn->connect_error);
 } else {
-    $sql = "SELECT COUNT(gender) as num FROM students GROUP BY gender ORDER BY gender DESC";
+    $sql = "SELECT gender, COUNT(gender) as num FROM students GROUP BY gender ORDER BY gender DESC";
     $result = $conn->query($sql);
     echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
 }
